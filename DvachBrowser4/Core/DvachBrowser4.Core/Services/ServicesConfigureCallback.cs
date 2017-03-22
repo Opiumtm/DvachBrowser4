331fa2041738c4e009eeb882a6b11b7bc5f4175d
Opiumtm <opium_tm@live.ru>
2017-03-22 21:58:39 +0300

    Dependency Injection

---
+++ DvachBrowser4/Core/DvachBrowser4.Core/Services/ServicesConfigureCallback.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DvachBrowser4.Core.Services
+{
+    /// <summary>
+    /// Обратный вызов для конфигурации сервисов в коллекции.
+    /// </summary>
+    /// <param name="serviceCollection">Коллекция сервисов.</param>
+    public delegate void ServicesConfigureCallback(IServiceCollection serviceCollection);
+}
