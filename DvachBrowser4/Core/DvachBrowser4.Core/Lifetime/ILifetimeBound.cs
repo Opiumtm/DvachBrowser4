b2bf79019a20e4b9428b19621671e800445d8bdc
Opiumtm <opium_tm@live.ru>
2017-03-23 21:58:48 +0300

    Управление жизненным циклом объектов

---
+++ DvachBrowser4/Core/DvachBrowser4.Core/Lifetime/ILifetimeBound.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DvachBrowser4.Core.Lifetime
+{
+    /// <summary>
+    /// Объект, имеющий управление циклом жизни.
+    /// </summary>
+    public interface ILifetimeBound
+    {
+        /// <summary>
+        /// Установить состояние цикла жизни объекта.
+        /// </summary>
+        /// <param name="state">Состояние.</param>
+        /// <returns>Таск.</returns>
+        Task SetLifetimeState(LifetimeState state);
+
+        /// <summary>
+        /// Состоянике цикла жизни.
+        /// </summary>
+        LifetimeState LifetimeState { get; }
+
+        /// <summary>
+        /// Можно устанавливать состояние.
+        /// </summary>
+        bool CanBindState { get; }
+
+        /// <summary>
+        /// Состояние изменилось.
+        /// </summary>
+        event EventHandler<LifetimeState> StateChanged;
+
+        /// <summary>
+        /// Присоединить к объекту.
+        /// </summary>
+        /// <param name="sourceObject">Исходный объект.</param>
+        void Bind(object sourceObject);
+
+        /// <summary>
+        /// Отсоединить от объекта.
+        /// </summary>
+        /// <param name="sourceObject">Исходный объект.</param>
+        void Unbind(object sourceObject);
+    }
+}
---
+++ DvachBrowser4/Core/DvachBrowser4.Core/Lifetime/LifetimeState.cs
@@ -0,0 +1,25 @@
+﻿namespace DvachBrowser4.Core.Lifetime
+{
+    /// <summary>
+    /// Состояние цикла жизни.
+    /// </summary>
+    public enum LifetimeState
+    {
+        /// <summary>
+        /// Не активен.
+        /// </summary>
+        Inactive,
+        /// <summary>
+        /// Активен.
+        /// </summary>
+        Active,
+        /// <summary>
+        /// Приостановлен.
+        /// </summary>
+        Suspended,
+        /// <summary>
+        /// Уничтожен (конечная фаза, дальнейшие изменения состояния невозможны).
+        /// </summary>
+        Disposed
+    }
+}
